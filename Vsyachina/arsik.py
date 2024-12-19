6a1117b4f854a1a8ab50ce85f7f60baf9a8990a0
ArsTyzykhyan <ego1st.jc@gmail.com>
2024-12-19 21:01:07 +0300

    module_8_3

---
+++ Vsyachina/arsik.py
@@ -0,0 +1,11 @@
+# 1st program
+print((9 ** 0.5) * 5)
+# 2nd program
+if 9.99 > 9.98 and 1000 != 1000.1:
+    print(True)
+# 3rd program
+a = 2 * 2 + 2
+print(a)
+b =(2 * (2 + 2))
+print(b)
+print(a == b)
