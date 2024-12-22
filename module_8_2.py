2fdc22a5b5861965fe3ae1c9eb34d1854e5e4b98
ArsTyzykhyan <ego1st.jc@gmail.com>
2024-12-22 20:31:57 +0300

    module_8_2

--- module_8_2.py
+++ module_8_2.py
@@ -2,24 +2,25 @@ def personal_sum(numbers):
     result = 0
     incorrect_data = 0
     for number in numbers:
-        try:
+        if isinstance(number, (int, float)):
             result += number
-        except TypeError:
+        else:
             print(f'Некорректный тип данных для подсчёта суммы - {number}')
             incorrect_data += 1
     return result, incorrect_data
-
-def calculate_average(numbers):
+def calculate_average(*numbers):
+    if isinstance(*numbers, int):
+        return None
     try:
-        sum, incorrect_data = personal_sum(numbers)
-        return sum / len(numbers)
+        tuple_pers_sum = personal_sum(*numbers)
+        return tuple_pers_sum[0] / (len(*numbers) - tuple_pers_sum[1])
     except ZeroDivisionError:
         return 0
     except TypeError:
-        print('В numbers записан некорректный тип данных')
-        return None
-
+        return f'В numbers записан некорректный тип данных'
 print(f'Результат 1: {calculate_average("1, 2, 3")}')
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
 print(f'Результат 3: {calculate_average(567)}')
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
+
+
